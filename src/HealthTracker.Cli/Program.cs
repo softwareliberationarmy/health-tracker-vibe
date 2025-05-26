650d9ed2c66cecda83644e790d10c46859bb97ac
Kerry Patrick <softwareliberationarmy@gmail.com>
2025-05-25 20:46:08 -0500

    feat: add log and view commands with subcommands to Health Tracker CLI
    - Implemented 'log' command to log health data with a subcommand for logging weight.
    - Implemented 'view' command to view health data with a subcommand for viewing weight.
    - Integrated both commands into the root command of the Health Tracker CLI.

--- src/HealthTracker.Cli/Program.cs
+++ src/HealthTracker.Cli/Program.cs
@@ -2,6 +2,40 @@
 
 var rootCommand = new RootCommand("Health Tracker CLI");
 
+// Create log command
+var logCommand = new Command("log", "Log health data");
+logCommand.SetHandler(() =>
+{
+    Console.WriteLine("log command invoked");
+});
+
+// Create log weight sub-subcommand
+var logWeightCommand = new Command("weight", "Log weight data");
+logWeightCommand.SetHandler(() =>
+{
+    Console.WriteLine("log weight command invoked");
+});
+logCommand.AddCommand(logWeightCommand);
+
+// Create view command
+var viewCommand = new Command("view", "View health data");
+viewCommand.SetHandler(() =>
+{
+    Console.WriteLine("view command invoked");
+});
+
+// Create view weight sub-subcommand
+var viewWeightCommand = new Command("weight", "View weight data");
+viewWeightCommand.SetHandler(() =>
+{
+    Console.WriteLine("view weight command invoked");
+});
+viewCommand.AddCommand(viewWeightCommand);
+
+// Add subcommands to root command
+rootCommand.AddCommand(logCommand);
+rootCommand.AddCommand(viewCommand);
+
 // System.CommandLine automatically provides --version support based on assembly version
 rootCommand.SetHandler(() =>
 {
